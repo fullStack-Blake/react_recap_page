8424c6b736680c52cd5d9d3349e2a002c69ee797
Blake Han <han.sangyeup@gmail.com>
2021-10-22 00:56:14 -0400

    Write add function to increase state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-function App() {
-  return <div className="App"></div>;
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    count: 0
+  };
+  add = () => {
+    this.setState(current => ({
+      count: current.count + 1
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.add}>ADD</button>
+        <button>MINUS</button>
+      </>
+    );
+  }
 }
 
 export default App;
