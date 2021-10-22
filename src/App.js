6a169b079eef851cd94a0b58e358b641902b8e82
Blake Han <han.sangyeup@gmail.com>
2021-10-22 02:09:44 -0400

    Use console.log to see Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Constructed!");
+  }
+  componentDidMount() {
+    console.log("1");
+  }
+  componentDidUpdate() {
+    console.log("2");
+  }
+  componentWillUnmount() {
+    console.log("Bye");
+  }
   state = {
     count: 0
   };
@@ -12,7 +25,9 @@ class App extends Component {
   minus = () => {
     this.setState(current => ({ count: current.count - 1 }));
   };
+
   render() {
+    console.log("Rendering!");
     return (
       <>
         <h1>{this.state.count}</h1>
