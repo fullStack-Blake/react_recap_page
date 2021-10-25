5f8312efa50e8524f29d6a4277f6c9d0bdaaf2ce
Blake Han <han.sangyeup@gmail.com>
2021-10-25 13:00:05 -0400

    Add styled-components in Brewery component

--- src/Brewery.js
+++ src/Brewery.js
@@ -1,8 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Name = styled.h2``;
+const Location = styled.h4``;
+const Phone = styled.p``;
+const Website = styled.a``;
 
 const Brewery = ({ name, street, city, state, phone, website }) => {
-  return <div>{name}</div>;
+  return (
+    <>
+      <Name>{name}</Name>
+      <Location>
+        {street}, {city}
+      </Location>
+      <Phone>{phone}</Phone>
+      <Website>{website}</Website>
+    </>
+  );
 };
 
 Brewery.propTypes = {
