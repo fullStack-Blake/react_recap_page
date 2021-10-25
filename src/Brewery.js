7c5b8fae6e9eb902729e94dd89d11809bf669262
Blake Han <han.sangyeup@gmail.com>
2021-10-25 00:42:13 -0400

    Using prop-types to check props in Brewery Component

--- src/Brewery.js
+++ src/Brewery.js
@@ -1,4 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Brewery = () => {};
+const Brewery = ({ name, street, city, state, phone, website }) => {};
+
+Brewery.propTypes = {
+  name: PropTypes.string.isRequired,
+  street: PropTypes.string.isRequired,
+  city: PropTypes.string,
+  state: PropTypes.string,
+  phone: PropTypes.string,
+  website: PropTypes.string
+};
